219ffa07396a2c50acfb93d0c8c2da475544ab1c
Adarsh <adarshchaudhary434@gmail.com>
2020-06-21 12:59:58 +0530

    add more route and controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-console.log('router loaded');
+console.log('router1 loaded');
 router.get('/', homeController.home);
-
+router.use('/users', require('./users'))
 module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/users_controller');
+console.log('router2 loaded');
+router.get('/profile', userController.profile);
+// router.get('/news', userController.news);
+// router.get('/feed', userController.feed);
+
+module.exports = router;
