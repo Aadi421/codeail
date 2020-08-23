f475671d7658e741f758dd0aba70dcbe8b96922e
Adarsh <adarshchaudhary434@gmail.com>
2020-08-23 23:21:22 +0530

    deletion of post along with their comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,5 @@ module.exports.create = function(req, res){
 
     });
 } 
+
+
--- models/post.js
+++ models/post.js
@@ -7,13 +7,13 @@ const postSchema = new mongoose.Schema({
         required: true
     },
     user: {
-        type:  mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
 
     },
     // include the array of ids of all comment in this post schema itself
     comments:[ {
-        type:  mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment'
 
     } ]
