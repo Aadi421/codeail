4c2a7d2d2e55d08d0ab28c2dbdc0b9a53dad18fe
Adarsh <adarshchaudhary434@gmail.com>
2020-09-04 13:38:24 +0530

    use of async await :in controller to escaping nesting of callback function (called callback hell) and clean code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,31 @@
 const Post=require('../models/post');
 const User=require('../models/user');
 
-module.exports.home = (req, res) => {
+module.exports.home = async (req, res) => {
     // console.log(req.cookies);
     // res.cookie('user_id ', 11)
-    // Post.find({},(err,posts)=>{
-    //     return res.render('home', {
-    //         title: 'Codiel | home',
-    //         posts:posts
-    //     });
-    // });
-    
+try{
     // populate the user of each post
-   Post.find({})
+   let posts=await Post.find({})
    .populate('user')
    .populate({
        path:'comments',
        populate:{
            path:'user'
        }
-   })
-   .exec((err,posts)=>{
+   });
+   
+   let users=await  User.find({});
 
-    User.find({},function (err,users) {
         return res.render('home',{
             title:"Codeial | Home",
             posts:posts,
             all_users:users
         });
-    });
-    
-})
+   }catch(err){
+       console.log('Error',err);
+       return;
+   }
 
 
 }
