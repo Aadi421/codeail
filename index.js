692b0d446cdaffe7ffe8ec26356d58636f870961
Adarsh <adarshchaudhary434@gmail.com>
2020-12-24 12:59:14 +0530

    media qureies update2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const customMware=require('./config/middleware');
 // setup the chat server to be used with socket.io
 const chatServer = require('http').Server(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
+chatServer.listen(process.env.PORT || 5000);
 console.log('chat server is listening on port 5000');
 
 
